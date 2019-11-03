4553affdc28257f8f10cd25cf1e57c85d839f758
Andrey Andreev <aandreevh@gmail.com>
2019-11-03 12:50:32 +0200

    Created fs

---
+++ src/fs.rkt
@@ -0,0 +1,107 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(define (file->metadata file) (car file))
+(define (file->name file) (car (file->metadata file)))
+(define (file->parent file) (cdr (file->metadata file)))
+(define (file->content file) (cdr file))
+
+(define (file? file [deep #f] )
+  (and (pair? file)
+       (pair? (file->metadata file))
+       (string? (file->name file))
+       (if (not deep) #t
+	    (or (null? (file->parent file))
+		       (file? (file->parent file))) ))) 
+
+(define (file->check? op)
+  (lambda (file [deep #f]) 
+    (and (file? file deep) (op (file->content file)))))
+
+(define file->regular? (file->check? string?))
+(define file->directory? (file->check? hash?))
+(define (file->root? file [deep #f]) (and (file? file deep) (null? (file->parent file))))
+
+(define (file->contains? file data)
+  (if (not (string? data)) #f
+  (cond ( (file->regular? file)
+	  (string-contains? (file->content file) data) )
+	((file->directory? file)
+	 (hash-has-key? (file->content file) data))
+	 (else #f))))
+
+(define (file->dir-get directory filename)
+  (if (and (file->directory? directory) 
+	   (hash-has-key? (file->content directory) filename)
+	   (string? filename))
+    (hash-ref (file->content directory) filename)
+    '()))
+
+(define (file->root) (cons (cons "" '()) (make-hash)))
+
+(define (file->create parent name content) 
+  (if 
+    (and (file->directory? parent) (string? name) 
+	 (or (string? content) (hash-empty? content)))
+    (if (file->contains? parent name) 
+      '()
+      (begin
+     (hash-set! (file->content parent) name (cons (cons name parent) content) )
+     (hash-ref (file->content parent) name)))
+    '()))
+
+(define (file->regular parent name data) 
+  (if (string? data) 
+    (file->create parent name data) 
+    '()))
+
+(define (file->directory parent name) (file->create parent name (make-hash)))
+
+
+(define (file->remove file)
+  (if (file? file)
+    (begin
+      (hash-remove! (file->content (file->parent file)) (file->name file))
+      (file->parent file)
+      ) '() ))
+
+(define (file->sub directory pred?)
+  (if (not (file->directory? directory)) '()
+	(filter (lambda (e) (not (null? e)))
+		(hash-map (file->content directory)
+		       (lambda (name file) 
+			 (if (pred? file)  file
+			   '())) ))))
+
+(define (file->sub-directory directory) (file->sub directory file->directory?))
+(define (file->sub-regular directory) (file->sub directory file->regular?))
+
+;(fs-root ; current)
+(define (context->create fs)
+  (cons fs fs))
+
+(define (context->root cntx)
+  (car cntx))
+
+(define (context->cur cntx)
+  (cdr cntx))
+
+(define (explorer->path-resolver path)
+  (string-split path "/"))
+
+(define (explorer->root-path? path)
+  (string-prefix? path "/"))
+
+(define (explorer->find context path)
+  (define managed-path (explorer->path-resolver path))
+  (define (helper cur mp)
+    (if (null? mp) cur
+      (if (not (file->directory? cur)) 
+	'()
+	(helper (file->dir-get cur (car mp)) (cdr mp)))))
+  (if (explorer->root-path? path)
+    (helper (context->root context) managed-path)
+    (helper (context->cur context) managed-path)))
+
+
--- src/main.rkt
+++ src/main.rkt
@@ -1,3 +1,13 @@
 #lang racket
+(require "fs.rkt")
 
-(display "Hello")
+(define fs (file->root))
+(define f1 (file->regular fs "f1" "data"))
+(define d1 (file->directory fs "1"))
+(define d2 (file->directory d1 "2"))
+(define d3 (file->directory d2 "3"))
+(define d4 (file->directory d3 "4"))
+(define d5 (file->directory d2 "5"))
+(define f2 (file->regular d5 "f2" "lala"))
+
+(file->content (explorer->find (cons fs d2) "/1/2/5/f2"))
